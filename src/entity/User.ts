169a770b7e3995cb52d01a5f99dc3d34b39d13fc
Sankara Ekane <sankaraekane@Sankaras-MacBook-Pro.local>
2019-12-25 22:48:00 -0500

    User Model, and Index

---
+++ src/entity/User.ts
@@ -0,0 +1,26 @@
+import {
+  BaseEntity,
+  BeforeInsert,
+  Column,
+  Entity,
+  PrimaryColumn,
+  PrimaryGeneratedColumn
+} from "typeorm";
+import uuidv4 = require("uuid/v4");
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryColumn("uuid")
+  id!: string;
+
+  @Column("varchar", { length: 255 })
+  email!: string;
+
+  @Column("text")
+  password!: string;
+
+  @BeforeInsert()
+  addId() {
+    this.id = uuidv4();
+  }
+}
