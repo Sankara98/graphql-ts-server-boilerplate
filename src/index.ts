f060b90fd10e2fe07e50f18dd210d9a25bc656af
Sankara Ekane <sankaraekane@Sankaras-MacBook-Pro.local>
2019-12-25 23:35:47 -0500

    Fixed path for graphql schema added connection to the psql database

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,13 @@ import "reflect-metadata";
 import { GraphQLServer } from "graphql-yoga";
 import { importSchema } from "graphql-import";
 import { resolvers } from "./resolvers";
+import * as path from "path";
+import { createConnection } from "typeorm";
 
-const typeDefs = importSchema("schema.graphql");
+const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"));
+
 const server = new GraphQLServer({ typeDefs, resolvers });
-server.start(() => console.log("Server is running on localhost:4000"));
+// tslint:disable-next-line: variable-name
+createConnection().then(_connection => {
+  server.start(() => console.log("Server is running on localhost:4000"));
+});
