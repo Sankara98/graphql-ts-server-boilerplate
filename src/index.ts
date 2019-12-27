3db79b6907c95412e56c123401011f091a063bbc
Sankara Ekane <sankaraekane@Sankaras-MacBook-Pro.local>
2019-12-27 04:28:17 -0500

    Created test for User registation:
    Test Checks for creation of the user in the database

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
 import "reflect-metadata";
-import { GraphQLServer } from "graphql-yoga";
-import { importSchema } from "graphql-import";
-import { resolvers } from "./resolvers";
-import * as path from "path";
-import { createConnection } from "typeorm";
+import { startServer } from "./startServer";
 
-const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"));
-
-const server = new GraphQLServer({ typeDefs, resolvers });
-// tslint:disable-next-line: variable-name
-createConnection().then(_connection => {
-  server.start(() => console.log("Server is running on localhost:4000"));
-});
+startServer();
